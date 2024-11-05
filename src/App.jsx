390dc192e346d56bca21f16ec334438569aa1c9e
naminlee <nm0922@catholic.ac.kr>
2024-11-05 15:19:37 +0900

    [Feat] 대시보드 컴포넌트 UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Sidebar from './components/SideBar/Sidebar';
+import MainContent from './components/MainContent/MainContent';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{ fontFamily: 'Arial, sans-serif', display: 'flex', justifyContent: "column", height: '100vh', width: '100vw'}}>
+      <Sidebar />
+      <MainContent />
+    </div>
     </>
   )
 }
