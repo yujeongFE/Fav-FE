0aa1034fd2b08bc9b081c81fe9839480255902ef
이나민 <nm0922@catholic.ac.kr>
2024-11-06 15:24:05 +0900

    [Feat] 사이드바 로고 수정

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -26,15 +26,18 @@ const Sidebar = () => {
           alignItems: "center",
           marginBottom: "40px",
         }}>
-        <div
+        <div>
+        <img
+          src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-BpDKq494k2vGEAtePMrbcfdRmb8N5d.png"
           style={{
             width: "32px",
             height: "32px",
-            backgroundColor: "#0066FF",
-            borderRadius: "8px",
-            marginRight: "12px",
-          }}></div>
-        <span style={{ fontWeight: "bold", fontSize: "18px" }}>Fav</span>
+            borderRadius: "50%",
+            objectFit: "cover",
+          }}
+        />
+        </div>
+        <span style={{ fontWeight: "bold", fontSize: "18px", position: "relative", top: "2px", }}>Fav</span>
       </div>
 
       <nav>
