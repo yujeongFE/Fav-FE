35aeb9b7f4d549e022ccd38327bb6df806b9dc7e
yujeongFE <109844803+yujeongFE@users.noreply.github.com>
2024-11-06 13:44:40 +0900

    fix: 사이드바 active 상태 확인 기능 추가

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
   const navigate = useNavigate();
-  const [activeNav, setActiveNav] = useState("");
+  const location = useLocation();
+  const [activeNav, setActiveNav] = useState("dashboard");
 
-  const handleClickBoard = (path) => {
-    setActiveNav(path);
+  useEffect(() => {
+    // URL path에 따라 activeNav를 설정
+    if (location.pathname === "/board") {
+      setActiveNav("board");
+    } else if (location.pathname === "/dashboard") {
+      setActiveNav("dashboard");
+    }
+  }, [location.pathname]);
+
+  const handleClickBoard = () => {
+    setActiveNav("board");
     navigate("/board");
+  };
+
+  const handleClickDash = () => {
+    setActiveNav("dashboard");
+    navigate("/dashboard");
   };
 
   return (
@@ -27,29 +41,31 @@ const Sidebar = () => {
           marginBottom: "40px",
         }}>
         <div>
-        <img
-          src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-BpDKq494k2vGEAtePMrbcfdRmb8N5d.png"
-          alt="커피 컵 로고"
-          style={{
-            width: "32px",
-            height: "32px",
-            borderRadius: "50%",
-            objectFit: "cover",
-          }}
-        />
+          <img
+            src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-BpDKq494k2vGEAtePMrbcfdRmb8N5d.png"
+            alt="커피 컵 로고"
+            style={{
+              width: "32px",
+              height: "32px",
+              borderRadius: "50%",
+              objectFit: "cover",
+            }}
+          />
         </div>
-        <span style={{ 
-          fontWeight: "bold", 
-          fontSize: "18px",
-          position: "relative",
-          top: "2px",
-           }}
-        >Fav</span>
+        <span
+          style={{
+            fontWeight: "bold",
+            fontSize: "18px",
+            position: "relative",
+            top: "2px",
+          }}>
+          Fav
+        </span>
       </div>
 
       <nav>
         <button
-          onClick={() => setActiveNav("dashboard")}
+          onClick={handleClickDash}
           style={{
             display: "flex",
             alignItems: "center",
@@ -66,7 +82,7 @@ const Sidebar = () => {
           <span style={{ marginRight: "10px", color: "black" }}>📊 대시보드</span>
         </button>
         <button
-          onClick={() => handleClickBoard("board")}
+          onClick={handleClickBoard}
           style={{
             display: "flex",
             alignItems: "center",
